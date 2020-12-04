afb410bc6aab963cff95ab39ff89ee7439d632e6
Trond Kandal <Trond.Kandal@ntnu.no>
2020-12-04 02:54:22 +0100

    Make a buffered work-queue.

--- workgroup.go
+++ workgroup.go
@@ -6,20 +6,20 @@ import (
 
 // WorkGroup runs a given number of functions concurrently.
 type WorkGroup struct {
-	main  chan func()
+	workQueue  chan func()
 	waitg *sync.WaitGroup
 }
 
 // NewWorkGroup allocates and returns a new WorkGroup.
 func NewWorkGroup(n int) *WorkGroup {
 	wg := &WorkGroup{
-		main:  make(chan func()),
+		workQueue:  make(chan func(), n),
 		waitg: &sync.WaitGroup{},
 	}
 
 	for i := 0; i < n; i++ {
 		go func() {
-			for f := range wg.main {
+			for f := range wg.workQueue {
 				f()
 				wg.waitg.Done()
 			}
@@ -31,11 +31,11 @@ func NewWorkGroup(n int) *WorkGroup {
 // Add adds a new worker function to execute.
 func (wg *WorkGroup) Add(f func()) {
 	wg.waitg.Add(1)
-	wg.main <- f
+	wg.workQueue <- f
 }
 
 // Wait waits until all worker function are finished.
 func (wg *WorkGroup) Wait() {
 	wg.waitg.Wait()
-	close(wg.main)
+	close(wg.workQueue)
 }
